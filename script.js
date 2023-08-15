e239ef34e1eacfd966168da868df684c53440c72
Am1d1v <124504237+Am1d1v@users.noreply.github.com>
2023-08-15 14:28:02 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -8,6 +8,18 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--temp');
 const inputElevation = document.querySelector('.form__input--climb');
 
+if(navigator.geolocation){
+    navigator.geolocation.getCurrentPosition(
+        function(position){
+            console.log(position);
+    },
+        function(){
+            alert('Cannot get your coordinates');
+        }
+    );
+    
+}
+
 
 
 
