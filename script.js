44b91a7befd84a39a582df9a5d6dba696e35a6ad
Am1d1v <124504237+Am1d1v@users.noreply.github.com>
2023-08-15 18:52:27 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -5,19 +5,21 @@ const containerWorkouts = document.querySelector('.workouts');
 const inputType = document.querySelector('.form__input--type');
 const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
-const inputCadence = document.querySelector('.form__input--temp');
-const inputElevation = document.querySelector('.form__input--climb');
+const inputTemp = document.querySelector('.form__input--temp');
+const inputClimb = document.querySelector('.form__input--climb');
+
+let map;
+let mapEvent;
 
 if(navigator.geolocation){
     navigator.geolocation.getCurrentPosition(
         function(position){
             const {latitude} = position.coords;
             const {longitude} = position.coords;
-            console.log(latitude, longitude);
-            console.log(`https://www.google.com/maps/@${latitude},${longitude},12z`);
+          //  console.log(latitude, longitude);
+          //  console.log(`https://www.google.com/maps/@${latitude},${longitude},12z`);
 
             const coords = [latitude, longitude];
-
             const map = L.map('map').setView(coords, 12);
 
             L.tileLayer('https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png', {
@@ -26,25 +28,38 @@ if(navigator.geolocation){
              }).addTo(map);
 
 
-            map.on('click', (mapEvent)=>{
-                //console.log(mapEvent);
-                const {lat, lng} = mapEvent.latlng;
-
-
-                L.marker([lat, lng])
-                .addTo(map)
-                .bindPopup(L.popup({
-                    maxWidth: 300,
-                    minWidth: 50,
-                    autoClose: false,
-                    closeOnClick: false,
-                    className: 'running-popup'
-                }))
-                .setPopupContent('String')
-                .openPopup();
+            map.on('click', (event)=>{
+                
+                mapEvent = event;
+                form.classList.remove('hidden');    
+                inputDistance.focus();
+ 
             });  
 
+            form.addEventListener('submit', function(event){
 
+                event.preventDefault();
+
+                // Input clearence
+                inputDistance.value = inputDuration.value  = inputTemp.value  = inputClimb.value  = '';
+            
+                // Marker view
+                const {lat, lng} = mapEvent.latlng;
+            
+            
+                L.marker([lat, lng])
+                    .addTo(map)
+                    .bindPopup(L.popup({
+                        maxWidth: 300,
+                        minWidth: 50,
+                        autoClose: false,
+                        closeOnClick: false,
+                        className: 'running-popup'
+                    }))
+                    .setPopupContent('Cardio')
+                    .openPopup();
+            
+            });
     },
         function(){
             alert('Cannot get your coordinates');
@@ -52,7 +67,10 @@ if(navigator.geolocation){
     );
 }
 
-
+inputType.addEventListener('change', () => {
+    inputClimb.closest('.form__row').classList.toggle('form__row--hidden');
+    inputTemp.closest('.form__row').classList.toggle('form__row--hidden');
+});
 
 
 
